e6c8ed166bd28f17beeda10637b1238c19d2a806
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-11-16 12:06:58 -0800

    shell

---
+++ checkEmptyFile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * empty_line - check for empty file
+ * Return: int
+ */
+int empty_line(char *buff)
+{
+	int i;
+
+	for (i = 0; buff[i] != '\0'; i++)
+	{
+		if (buff[i] != ' ')
+			return (0);
+	}
+	return (1);
+}
